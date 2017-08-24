6582e278f1f3fcbbd3329113c43f12345e7e9e10
muradheydarov <muradheyderov@gmail.com>
2017-08-24 20:36:52 +0400

    TextEditor

---
+++ README.md
@@ -0,0 +1,8 @@
+# P101-P201-01.06.2017
+Text Editor
+
+Əlavədəki şəkildə olan text editor xüsusiyyətləri minimal text editor xüsusiyyətlərdir.Bu xüsusiyyətlərə sahib olan bir text editor yazmaq tələb olunur.
+
+Qeyd- Özünüz əlavə xüsusiyyətlər de tətbiq edə bilsəniz sevindirmiş olarsınız.
+
+
---
+++ TextEditor/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace TextEditor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void newToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openDialog = new OpenFileDialog();
+            openDialog.Filter= "txt files (*.txt)|*.txt";
+            openDialog.Title = "Open a file...";
+            if (openDialog.ShowDialog()==DialogResult.OK)
+            {
+                StreamReader read = new StreamReader(openDialog.FileName);
+                richTextBox1.Text = read.ReadToEnd();
+                read.Close();
+            }
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog save = new SaveFileDialog();
+            save.Filter = "txt files (*.txt)|*.txt";
+            save.Title = "Save a file...";
+            if (save.ShowDialog()==DialogResult.OK)
+            {
+                StreamWriter write = new StreamWriter(save.FileName);
+                write.Write(richTextBox1.Text);
+                write.Close();
+            }
+        }
+
+        private void undoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Undo();
+        }
+
+        private void redoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Redo();
+        }
+
+        private void cutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Cut();
+        }
+
+        private void copyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Copy();
+        }
+
+        private void pasteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Paste();
+        }
+
+        private void selectAllToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.SelectAll();
+        }
+
+        private void fontDialog1_Apply(object sender, EventArgs e)
+        {
+
+        }
+
+        private void fontToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            DialogResult result = fontDialog1.ShowDialog();
+            if (result==DialogResult.OK)
+            {
+                Font font = fontDialog1.Font;
+                richTextBox1.Font = font;
+            }
+        }
+
+        private void colorToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            DialogResult result = colorDialog1.ShowDialog();
+            if (result==DialogResult.OK)
+            {
+                richTextBox1.ForeColor = colorDialog1.Color;
+            }
+        }
+    }
+}
